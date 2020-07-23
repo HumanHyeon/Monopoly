aaf9fda698151d39a4c4d7a51af6037b17aad038
HumanHyeon <54471641+HumanHyeon@users.noreply.github.com>
2020-07-23 15:06:56 +0900

    Create color_define.h

---
+++ color_define.h
@@ -0,0 +1,16 @@
+#define COL GetStdHandle(STD_OUTPUT_HANDLE)
+#define BLACK		SetConsoleTextAttribute(COL, 0x0000);	//검정색
+#define DARK_BLUE	SetConsoleTextAttribute(COL, 0x0001);	//파란색
+#define GREEN		SetConsoleTextAttribute(COL, 0x0002);	//녹색
+#define BLUE_GREEN	SetConsoleTextAttribute(COL, 0x0003);	//청녹색
+#define BLOOD		SetConsoleTextAttribute(COL, 0x0004);	//검붉은색
+#define PURPLE		SetConsoleTextAttribute(COL, 0x0005);	//보라색
+#define GOLD		SetConsoleTextAttribute(COL, 0x0006);	//금색
+#define ORIGINAL	SetConsoleTextAttribute(COL, 0x0007);	//밝은회색
+#define GRAY		SetConsoleTextAttribute(COL, 0x0008);	//회색
+#define BLUE		SetConsoleTextAttribute(COL, 0x0009);	//파란색
+#define HIGH_GREEN	SetConsoleTextAttribute(COL, 0x000a);	//연두색
+#define SKY_BLUE	SetConsoleTextAttribute(COL, 0x000b);	//하늘색
+#define RED			SetConsoleTextAttribute(COL, 0x000c);	//빨간색
+#define PLUM		SetConsoleTextAttribute(COL, 0x000d);	//자주색
+#define YELLOW		SetConsoleTextAttribute(COL, 0x000e);	//노란색
